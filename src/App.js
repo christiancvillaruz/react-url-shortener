bc2aecaecb68540c9d23188aace033a79d452f21
christiancvillaruz <42919026+christiancvillaruz@users.noreply.github.com>
2022-07-14 22:31:04 +0800

    updated app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Helmet } from 'react-helmet';
+import URLShortener from './components/URLShortener';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className="h-screen bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
+      <Helmet>
+        <title>URL Shortener</title>
+      </Helmet>
+      <div className="container mx-auto">
+        <URLShortener/>
+      </div>
     </div>
   );
 }
