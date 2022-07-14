a1e4eb9da1fbf4468d3a12a8887d2db632e87e0e
christiancvillaruz <42919026+christiancvillaruz@users.noreply.github.com>
2022-07-15 00:16:58 +0800

    updated app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { Helmet } from 'react-helmet';
 import URLShortener from './components/URLShortener';
 import './App.css';
+import { useState } from 'react';
+import ShortLink from './components/ShortLink';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
   return (
-    <div className="h-screen bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
+    <div className="min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
       <Helmet>
         <title>URL Shortener</title>
       </Helmet>
       <div className="container mx-auto">
-        <URLShortener/>
+        <div className="flex flex-col justify-center items-center min-h-screen">
+          <URLShortener setInputValue={ setInputValue } />
+          <div className="p-10">
+            <ShortLink inputValue={ inputValue } />
+          </div>
+        </div>
       </div>
     </div>
   );
