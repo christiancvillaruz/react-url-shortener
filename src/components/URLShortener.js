63592887d76da24222a29c07c5e9250e8e38ba44
christiancvillaruz <42919026+christiancvillaruz@users.noreply.github.com>
2022-07-15 00:17:15 +0800

    updated url shortener component

--- src/components/URLShortener.js
+++ src/components/URLShortener.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const URLShortener = () => {
+const URLShortener = ({ setInputValue }) => {
+  const [value, setValue] = useState("");
+
+  const handleClick = () => {
+    setInputValue(value);
+    setValue("");
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center p-3 h-screen">
+    <div className="w-3/6">
       <div className="text-center p-4">
         <h1 className="p-2 font-anton text-center text-5xl">URL  <span className="text-amber-400">Shortener</span></h1>
         <p className="font-quicksand text-center italic">Shorten your link in just one click!</p>
       </div>
-      <div className="bg-white rounded-sm p-4 w-6/12 font-quicksand text-slate-500">
+      <div className="bg-white rounded-sm p-6 h-44 font-quicksand text-slate-500">
         <form>
           <div class="mb-6">
-            <label for="url-text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-            <input type="url" id="url-text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://www.example.com" required />
+            <label for="url-text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your link</label>
+            <input type="url" id="url-text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://www.example.com"
+            value={ value }
+            onChange={ (e) => setValue(e.target.value) }
+             />
           </div>
-          <button type="submit" class="float-right text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+          <button type="button" className="float-right text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={ handleClick }>Shorten!</button>
         </form>
       </div>
     </div>
